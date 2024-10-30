1e37977024cfd321d0ed93a815d329e81d1944c6
Undergamer64 <baptiste.dufosse2005@gmail.com>
2024-10-30 14:47:03 +0100

    Reorganised files

---
+++ Assets/Scripts/ItemWithQuantity.cs
@@ -0,0 +1,8 @@
+using System;
+
+[Serializable]
+public struct ItemsWithQuantity
+{
+    public ItemBase _Item;
+    public int _Quantity;
+}
--- Assets/Scripts/Structures/Recipe.cs
+++ Assets/Scripts/Structures/Recipe.cs
@@ -1,14 +1,5 @@
-using System;
 using System.Collections.Generic;
 using UnityEngine;
-
-[Serializable]
-public struct ItemsWithQuantity
-{
-    public ItemBase _Item;
-    public int _Quantity;
-}
-
 
 [CreateAssetMenu(fileName = "Recipe", menuName = "ScriptableObjects/Recipe", order = 1)]
 public class Recipe : ScriptableObject
