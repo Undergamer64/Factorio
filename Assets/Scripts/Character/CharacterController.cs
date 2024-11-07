624193796032fa1792b48858d182ad9185fe5a85
Undergamer64 <baptiste.dufosse2005@gmail.com>
2024-11-07 10:21:38 +0100

    Fixed and finished tool bar
    finished the toolbar and fixed a bug where you could place behind the tool bar
    Also started to think about the preview

--- Assets/Scripts/Character/CharacterController.cs
+++ Assets/Scripts/Character/CharacterController.cs
@@ -1,5 +1,8 @@
+using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
+using UnityEngine.UI;
 
 public class CharacterController : MonoBehaviour
 {
@@ -44,7 +47,27 @@ public class CharacterController : MonoBehaviour
 
     public void MouseMovementAction(InputAction.CallbackContext context)
     {
+        Vector2 oldPos = _currentMousePosition;
         _currentMousePosition = context.ReadValue<Vector2>();
+
+        StructureItem structure = _characterData._PlacedStructureItem;
+        if (structure == null)
+        {
+            return;
+        }
+
+        Vector2 structurePos = _currentMousePosition;
+
+        if (_currentMousePosition == Vector2.zero)
+        {
+            if (oldPos ==  Vector2.zero)
+            { 
+                return; 
+            }
+            structurePos = oldPos;
+        }
+        
+
     }
 
     public void LeftClickAction(InputAction.CallbackContext context)
@@ -54,16 +77,22 @@ public class CharacterController : MonoBehaviour
             StructureItem itemStructure = _characterData._PlacedStructureItem;
             if (_currentMousePosition == Vector2.zero || itemStructure == null) { return; }
 
+            if (CheckUIInTheWay())
+            {
+                return;
+            }
+
             Vector3 mousePos = _camera.GetComponent<Camera>().ScreenToWorldPoint(_currentMousePosition);
 
-            RaycastHit2D hit = Physics2D.Raycast(mousePos + Vector3.back * 10, _camera.GetComponent<Camera>().transform.forward, 11f);
-            if (hit.collider != null || !TileManager._Instance.CanPlace(mousePos, itemStructure._SizeX, itemStructure._SizeY))
+            RaycastHit2D hit2D = Physics2D.Raycast(mousePos, _camera.GetComponent<Camera>().transform.forward, 1f);
+            if (hit2D.collider != null || !TileManager._Instance.CanPlace(mousePos, itemStructure._SizeX, itemStructure._SizeY))
             {
                 return;
             }
             
             if (itemStructure.Structure != null)
             {
+                //_characterData._Inventory.TryRemoveItems(itemStructure, 1);
                 TileManager._Instance.Place(itemStructure.Structure, itemStructure._SizeX, itemStructure._SizeY, mousePos);
             }
         }
@@ -74,6 +103,11 @@ public class CharacterController : MonoBehaviour
         if (context.started)
         {
             if (_currentMousePosition == Vector2.zero) { return; }
+            
+            if (CheckUIInTheWay())
+            {
+                return;
+            }
 
             Vector3 mousePos = _camera.GetComponent<Camera>().ScreenToWorldPoint(_currentMousePosition);
 
@@ -94,5 +128,20 @@ public class CharacterController : MonoBehaviour
 
             //_characterData._Inventory.TryAddItems(item);
         }
+    }
+
+    private bool CheckUIInTheWay()
+    {
+        PointerEventData customEventData = new PointerEventData(EventSystem.current);
+
+        customEventData.position = _currentMousePosition;
+        List<RaycastResult> results = new List<RaycastResult>();
+        EventSystem.current.RaycastAll(customEventData, results);
+
+        if (results.Count > 0)
+        {
+            return true;
+        }
+        return false;
     }
 }
